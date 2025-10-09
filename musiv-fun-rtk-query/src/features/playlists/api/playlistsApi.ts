6cfdc692d65c632ffc0f76e46c53e934611b4cb1
Mikhail8815 <mishutka150588@mail.ru>
2025-10-09 23:23:17 +0300

    Add fetchPlaylist query

---
+++ musiv-fun-rtk-query/src/features/playlists/api/playlistsApi.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type {
+    CreatePlaylistArgs,
+    FetchPlaylistsArgs, PlaylistData,
+    PlaylistsResponse
+} from "@/features/playlists/api/playlistsApi.types.ts";
+
+export const playlistsApi = createApi({
+    reducerPath: 'playlistsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_BASE_URL,
+        headers: {
+            'API-KEY': import.meta.env.VITE_API_KEY,
+        },
+    }),
+    endpoints: build => ({
+        fetchPlaylists: build.query<PlaylistsResponse, void>({
+            query: () => 'playlists',
+        }),
+        createPlaylist: build.mutation<{ data: PlaylistData }, CreatePlaylistArgs>({
+            query: (body) => {
+                return {
+                    method: 'POST',
+                    url: 'playlists',
+                    body
+                }
+            }
+        }),
+    }),
+})
+
+export const { useFetchPlaylistsQuery, useCreatePlaylistMutation } = playlistsApi
