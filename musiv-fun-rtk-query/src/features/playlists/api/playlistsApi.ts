4b641301cc53b07674aea8aded53d95ba741657d
Mikhail8815 <mishutka150588@mail.ru>
2025-10-16 20:00:33 +0300

    Add delete playlist, update playlist, decomposition

--- musiv-fun-rtk-query/src/features/playlists/api/playlistsApi.ts
+++ musiv-fun-rtk-query/src/features/playlists/api/playlistsApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type {
     CreatePlaylistArgs,
     FetchPlaylistsArgs, PlaylistData,
-    PlaylistsResponse
+    PlaylistsResponse, UpdatePlaylistArgs
 } from "@/features/playlists/api/playlistsApi.types.ts";
 
 export const playlistsApi = createApi({
@@ -11,6 +11,10 @@ export const playlistsApi = createApi({
         baseUrl: import.meta.env.VITE_BASE_URL,
         headers: {
             'API-KEY': import.meta.env.VITE_API_KEY,
+        },
+        prepareHeaders: headers => {
+            headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
+            return headers
         },
     }),
     endpoints: build => ({
@@ -26,7 +30,24 @@ export const playlistsApi = createApi({
                 }
             }
         }),
+        deletePlaylist: build.mutation<void, string>({
+            query: (playlistId) => {
+                return {
+                    method: 'DELETE',
+                    url: `playlists/${playlistId}`,
+                }
+            }
+        }),
+        updatePlaylist: build.mutation<void, { playlistId: string, body: UpdatePlaylistArgs }>({
+            query: ({playlistId, body}) => {
+                return {
+                    method: 'PUT',
+                    url: `playlists/${playlistId}`,
+                    body
+                }
+            }
+        }),
     }),
 })
 
-export const { useFetchPlaylistsQuery, useCreatePlaylistMutation } = playlistsApi
+export const { useFetchPlaylistsQuery, useCreatePlaylistMutation, useDeletePlaylistMutation, useUpdatePlaylistMutation } = playlistsApi
--- musiv-fun-rtk-query/src/features/playlists/ui/PlaylistsPage.tsx
+++ musiv-fun-rtk-query/src/features/playlists/ui/PlaylistsPage.tsx
@@ -1,20 +1,71 @@
-import {useFetchPlaylistsQuery} from "@/features/playlists/api/playlistsApi.ts";
+import {
+    useDeletePlaylistMutation,
+    useFetchPlaylistsQuery,
+} from "@/features/playlists/api/playlistsApi.ts";
 import s from './PlaylistPage.module.css'
+import {CreatePlaylistForm} from "@/features/playlists/ui/CreatePlaylistForm/CreatePlaylistForm.tsx";
+import {useForm} from "react-hook-form";
+import type {
+    PlaylistData,
+    UpdatePlaylistArgs
+} from "@/features/playlists/api/playlistsApi.types.ts";
+import {useState} from "react";
+import {PlaylistItem} from "@/features/playlists/ui/PlaylistItem/PlaylistItem.tsx";
+import {EditPlaylistForm} from "@/features/playlists/ui/EditPlaylistForm/EditPlaylistForm.tsx";
 
 export const PlaylistsPage = () => {
 
+    const [playlistId, setPlaylistId] = useState<string | null>(null)
+
+    const { register, handleSubmit, reset } = useForm<UpdatePlaylistArgs>()
+
     const {data} = useFetchPlaylistsQuery();
+    const [deletePlaylist] = useDeletePlaylistMutation()
+
+
+    const deletePlaylistHandler = (playlistId: string) => {
+        if (confirm('Are you sure you want to delete the playlist?')) {
+            deletePlaylist(playlistId)
+        }
+    }
+
+    const editPlaylistHandler = (playlist: PlaylistData | null) => {
+        if (playlist) {
+            setPlaylistId(playlist.id)
+            reset({
+                title: playlist.attributes.title,
+                description: playlist.attributes.description,
+                tagIds: playlist.attributes.tags.map(t => t.id),
+            })
+        } else {
+            setPlaylistId(null)
+        }
+    }
 
     return (
         <div className={s.container}>
             <h1>Playlists page</h1>
+            <CreatePlaylistForm />
             <div className={s.items}>
                 {data?.data.map(playlist => {
+                    const isEditing = playlistId === playlist.id
+
                     return (
                         <div className={s.item} key={playlist.id}>
-                            <div>title: {playlist.attributes.title}</div>
-                            <div>description: {playlist.attributes.description}</div>
-                            <div>userName: {playlist.attributes.user.name}</div>
+                            {isEditing ? (
+                                <EditPlaylistForm
+                                    playlistId={playlistId}
+                                    setPlaylistId={setPlaylistId}
+                                    editPlaylist={editPlaylistHandler}
+                                    register={register}
+                                    handleSubmit={handleSubmit}
+                                />
+                            ) : (
+                                <PlaylistItem playlist={playlist}
+                                              deletePlaylistHandler={deletePlaylistHandler}
+                                              editPlaylistHandler={editPlaylistHandler}
+                                />
+                            )}
                         </div>
                     )
                 })}
